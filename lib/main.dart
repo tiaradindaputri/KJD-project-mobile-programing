f8a8c4fd4eed92cb1d7241ecf50f08d39c56607a
tiaradindaputri <tiaradindaputri54@gmal.com>
2024-10-12 20:36:36 +0700

    Menambahkan halaman login

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
-import 'login.dart'; // Import halaman login
-import 'home.dart'; // Import halaman home
+import 'menu login.dart'; // Import halaman login
 
 void main() {
   runApp(MyApp());
@@ -14,10 +13,8 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: LoginPage(), // Halaman pertama yang ditampilkan adalah halaman login
-      routes: {
-        '/home': (context) => HomePage(), // Routing ke halaman home
-      },
+      home: LoginPage(), // Halaman yang ditampilkan pertama kali adalah halaman login
     );
   }
 }
+S
